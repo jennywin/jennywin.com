306049b65fd9ec313315fc058c5e7016c4482e17
Jenny Nguyen <jnguyen72086@gmail.com>
2017-12-03 21:49:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jennywin.com
+Built with [Gatsby] (https://github.com/gatsbyjs/gatsby) and [gatsby-starter-grommet] (https://github.com/alampros/gatsby-starter-grommet)
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Login from '../components/Login'
+import Main from '../components/Main'
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    )
+  }
+}
