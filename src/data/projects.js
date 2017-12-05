db0e218092b9907ff0168ef6fe3113274381f844
Jenny Nguyen <jnguyen72086@gmail.com>
2017-12-04 22:56:34 -0800

    Refactor projects page

---
+++ src/data/projects.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import CardImageTPL from '../assets/projects/the-petite-life.png'
+import CardImageGX from '../assets/projects/globalxplorer.png'
+import CardImageBG from '../assets/projects/brew-guru.jpg'
+import CardImageDH2 from '../assets/projects/dishonored2.png'
+import CardImageBS from '../assets/projects/blue-sky.png'
+import CardImageDAC from '../assets/projects/douglas-allred-company.png'
+import CardImageBMC from '../assets/projects/boulder-movement-collective.png'
+import CardImageEE from '../assets/projects/earthecho.png'
+
+export const ProjectsData = {
+  'projects': [
+    {
+      'name': 'The Petite Life',
+      'description': 'A community for petite people (Ruby on Rails).',
+      'url': 'http://www.thepetitelife.com',
+      'thumbnail': CardImageTPL
+    },
+    {
+      'name': 'GlobalXplorer',
+      'description': 'Web app for using satellite imagery to save human history (React).',
+      'url': 'http://www.globalxplorer.org',
+      'thumbnail': CardImageGX
+    },
+    {
+      'name': 'Brew Guru',
+      'description': 'Mobile app for the American Homebrewers Association (Ionic/Cordova/Angular 2).',
+      'url': 'https://www.homebrewersassociation.org/brew-guru',
+      'thumbnail': CardImageBG
+    },
+    {
+      'name': 'Dishonored 2',
+      'description': 'Free Trial for Bethesda game (Laravel).',
+      'url': 'http://dishonored.bethesda.net/trial',
+      'thumbnail': CardImageDH2
+    },
+    {
+      'name': 'Blue Sky Airport Parking',
+      'description': 'Managed branding and website development for Blue Sky Airport Parking.',
+      'url': 'http://www.parkbluesky.com',
+      'thumbnail': CardImageBS
+    },
+    {
+      'name': 'Douglas Allred Company',
+      'description': 'Managed redesign of website and print brochure.',
+      'url': 'http://www.douglasallredco.com',
+      'thumbnail': CardImageDAC
+    },
+    {
+      'name': 'Boulder Movement Collective',
+      'description': 'Strength and movement gym website (Squarespace and Front Desk).',
+      'url': 'http://www.bouldermovementcollective.com',
+      'thumbnail': CardImageBMC
+    },
+    {
+      'name': 'EarthEcho',
+      'description': 'Legacy homepage redesign for a non-profit focused on inspiring a sustainable future (ExpressionEngine).',
+      'url': 'http://www.earthecho.org',
+      'thumbnail': CardImageEE
+    },
+  ]
+}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,17 +6,25 @@ import Section from 'grommet/components/Section'
 import Tiles from 'grommet/components/Tiles'
 import Tile from 'grommet/components/Tile'
 import Card from 'grommet/components/Card'
+import Image from 'grommet/components/Image'
 
-import CardImage1 from '../assets/projects/the-petite-life.png'
-import CardImage2 from '../assets/projects/globalxplorer.png'
-import CardImage3 from '../assets/projects/brew-guru.jpg'
-import CardImage4 from '../assets/projects/dishonored2.png'
-import CardImage5 from '../assets/projects/blue-sky.png'
-import CardImage6 from '../assets/projects/douglas-allred-company.png'
-import CardImage7 from '../assets/projects/boulder-movement-collective.png'
-import CardImage8 from '../assets/projects/earthecho.png'
+import { ProjectsData } from '../data/projects.js'
 
 export default class Projects extends React.Component {
+  renderProjectTiles() {
+    return ProjectsData.projects.map(project => {
+      return (
+        <Tile separator='all'>
+          <Anchor target='_blank' href={project.url}>
+            <Card thumbnail={<Image src={project.thumbnail} fit='contain' />}
+              heading={project.name}
+              description={project.description} />
+          </Anchor>
+        </Tile>
+      )
+    })
+  }
+
   render() {
     return (
       <Section
@@ -31,63 +39,9 @@ export default class Projects extends React.Component {
           flush={false}
           pad='medium'
           justify='center'
+          responsive
         >
-          <Tile>
-            <Anchor target='_blank' href='http://www.thepetitelife.com'>
-              <Card thumbnail={CardImage1}
-                heading='The Petite Life'
-                description='A community for petite people (Ruby on Rails).' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='http://www.globalxplorer.org'>
-              <Card thumbnail={CardImage2}
-                heading='GlobalXplorer'
-                description='Web app for using satellite imagery to save human history (React).' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='https://www.homebrewersassociation.org/brew-guru/'>
-              <Card thumbnail={CardImage3}
-                heading='Brew Guru'
-                description='Mobile app for the American Homebrewers Association (Ionic/Cordova/Angular 2).' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='http://dishonored.bethesda.net/trial'>
-              <Card thumbnail={CardImage4}
-                heading='Dishonored 2'
-                description='Free Trial for Bethesda game (Laravel).' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='http://www.parkbluesky.com'>
-              <Card thumbnail={CardImage5}
-                heading='Blue Sky'
-                description='Managed branding and website development for Blue Sky Airport Parking.' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='http://www.douglasallredco.com'>
-              <Card thumbnail={CardImage6}
-                heading='Douglas Allred Company'
-                description='Managed redesign of website and print brochure.' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='http://www.bouldermovementcollective.com'>
-              <Card thumbnail={CardImage7}
-                heading='Boulder Movement Collective'
-                description='Strength and mobility gym website (Squarespace and Front Desk).' />
-            </Anchor>
-          </Tile>
-          <Tile>
-            <Anchor target='_blank' href='http://www.earthecho.org'>
-              <Card thumbnail={CardImage8}
-                heading='EarthEcho'
-                description='Legacy homepage redesign for a non-profit focused on inspiring a sustainable future (ExpressionEngine).' />
-            </Anchor>
-          </Tile>
+          {this.renderProjectTiles()}
         </Tiles>
       </Section>
     )
